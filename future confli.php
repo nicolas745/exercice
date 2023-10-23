6d3a5808784fa169d52acd7bed8834d324dce1c6
nicolas blanc <47307856+nicolas745@users.noreply.github.com>
2023-10-23 14:39:46 +0200

    v2

--- future confli.php
+++ future confli.php
@@ -1,11 +1,11 @@
 <?php
 function calc(a,b){
-    return a*b
+    return a*b*42
 }
 function calc2(a,b){
-    return a+b
+    return a/b
 }
 
 function calc3(a,b){
-    return a/b
+    return a+b
 }
